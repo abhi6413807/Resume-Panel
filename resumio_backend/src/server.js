1b601112057cbffa4500531b7658f39043c38217
Dhruv Khatri <dhruvkhatri839@gmail.com>
2026-07-10 15:40:21 +0530

    Ready for deployment

--- resumio_backend/src/server.js
+++ resumio_backend/src/server.js
@@ -71,7 +71,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
 const { initScheduler } = require("./utils/scheduler");
 
